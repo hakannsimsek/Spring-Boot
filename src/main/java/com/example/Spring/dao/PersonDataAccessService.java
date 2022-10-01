00f09ac5f0e31d02e3ea012e83fdfd685f707434
hhakann <74412503+hhakann@users.noreply.github.com>
2022-10-01 23:48:48 +0300

    Docker & PostgreSQL added by JDBC

--- src/main/java/com/example/Spring/dao/PersonDataAccessService.java
+++ src/main/java/com/example/Spring/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package com.example.Spring.dao;
 
 import com.example.Spring.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,13 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao{
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -16,12 +25,27 @@ public class PersonDataAccessService implements PersonDao{
 
     @Override
     public List<Person> SelectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sql = "Select id,name From person";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            UUID id = UUID.fromString(rs.getString("id"));
+            String name = rs.getString("name");
+            return new Person(id,name);
+        });
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "Select id, name From  person Where id = ?";
+        Person person = jdbcTemplate.queryForObject(
+                sql,
+                //{id}
+                new Object[]{id},
+                (rs, rowNum) -> {
+                    UUID personId = UUID.fromString(rs.getString("id"));
+                    String personName = rs.getString("name");
+                    return new Person(personId,personName);
+                });
+        return Optional.ofNullable(person);
     }
 
     @Override
